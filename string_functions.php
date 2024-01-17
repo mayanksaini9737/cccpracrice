1dfd3f5341b194fb2a808f530b731f2b35c18c15
mayanksaini9737 <113924903+mayanksaini9737@users.noreply.github.com>
2024-01-17 16:13:30 +0530

    Practiced strings functions

---
+++ string_functions.php
@@ -0,0 +1,55 @@
+<?php 
+    
+    // 1. strlen($string) - Returns the length of a string
+    // $msg = "Hello, Good afternoon";
+    // echo strlen($msg);
+    
+
+    // 2. str_replace($search, $replace, $subject) - Replaces all occurrences of a substring with another substring in a given string.
+    // $msg = "Hello, Good afternoon";
+    // echo str_replace("afternoon", "morning", $msg);
+
+
+    // 3. strpos($haystack, $needle) - Finds the position of the first occurrence of a substring in a string (index starts from 0), and also have a argument offset for starting search after that position
+    // $msg = "Hello, Good afternoon sir, Good afternoon madam";
+    // echo strpos($msg, "Good");
+    // echo strpos($msg, "Good", 15);
+
+    
+    // 4. substr($string, $start, $length) - Returns a part of a string starting from the specified position and with a specified length
+    // $msg = "Hello, Good afternoon";
+    // echo substr($msg, 7, 5);
+    // echo "<br>";
+    // echo substr($msg, 7);
+
+
+    // 5. strtolower($string) - Converts a string to lowercase
+    // echo strtolower("Hello, GUYS");
+
+
+    // 6. strtoupper($string) - Converts a string to uppercase
+    // echo strtoupper("hello deep");
+
+
+    // 7. trim($string) - Removes whitespace or other predefined characters from the beginning and end of a string
+    // echo trim("    Hello, Deep  ");
+    // echo trim("Hello, Deep", "Heep");
+
+
+    // 8. implode($glue, $pieces) or implode(seprator, arr) - returns a string from the elements of an array
+    // $arr = array("Hii", "Ravi", "How", "are", "you");
+    // echo implode(" ", $arr);
+
+    // 9. explode($delimiter, $string) - Splits a string into an array by a specified delimiter
+    
+
+
+
+    
+
+
+
+
+    
+
+?>
