53a092aeed51ce3d6774bfe0786482a30bfaa2af
mayanksaini9737 <113924903+mayanksaini9737@users.noreply.github.com>
2024-01-17 18:07:14 +0530

    More strings functions

--- string_functions.php
+++ string_functions.php
@@ -37,11 +37,77 @@
 
 
     // 8. implode($glue, $pieces) or implode(seprator, arr) - returns a string from the elements of an array
+    // join() is same as implode()
     // $arr = array("Hii", "Ravi", "How", "are", "you");
     // echo implode(" ", $arr);
+    // echo join(" ", $arr);
 
-    // 9. explode($delimiter, $string) - Splits a string into an array by a specified delimiter
+
+    // 9. explode($delimiter, $string) or explode(separator,string,limit) - Splits a string into an array by a specified delimiter
+    // limit(optional) - Greater than 0 - Returns an array with a maximum of limit element(s)
+                        // Less than 0 - Returns an array except for the last -limit elements()
+                        // 0 - Returns an array with one element
+    // $String = "Mayank, Deep, Yogeshwar, Azeem";
+    // print_r(explode(',', $String));
+    // print_r(explode(',', $String, 0));
+    // print_r(explode(',', $String, 4));
+    // print_r(explode(',', $String, -2));
+
+
+    // 10. htmlspecialchars($string) or htmlspecialchars(string,flags,optional character-set,optional double_encode)- Converts special characters to HTML entities like < to &lt;. It prevents from cross-site attack (XSS)
+    // doesn't direct show in browser shoeing in html file or check in browser using check page source 
+    // $str = "Make paragraph in html page using this syntax <p> This is a paragraph </p>";
+    // echo htmlspecialchars($str);
+    // $output = htmlspecialchars($str);
+    // echo htmlspecialchars($output);
+
+
+    // 11. htmlentities($string) or htmlentities(string,flags,character-set,double_encode) - Converts all applicable characters to HTML entities
+    // $str = "<div class='container'>This is a container </div>";
+    // echo htmlentities($str);
+    // $output = htmlentities($str);
+    // echo htmlentities($output);
+
+    // 12. str_repeat($string, $multiplier) - Repeats a string a specified number of time
+    // using this we can print pattern
+    // $str = "Good morning";
+    // $str2 = str_repeat($str, 5);
+    // echo $str2
+
+
+    // 13. strrev($string) - Reverses a string
+    // $str = "Good";
+    // echo strrev($str);
+
+
+    // 14. str_shuffle($string) - Randomly shuffles all characters in a string
+    // it is can be used in a shuffle game like card
+    // $str = "Randomly shuffles all characters in a string";
+    // echo str_shuffle($str);
+
+
+    // 15. str_split($string, $length) - Converts a string to an array, breaking it into smaller chunks
+    // $str = "Converts a string to an array";
+    // print_r(str_split($str)); // lengh default is 1
+    // print_r(str_split($str, 7)); 
+
+
+    // 16. str_word_count($string) - Returns the number of words in a string
+    // echo str_word_count("Returns the number of words in a string");
+
     
+    // 17. substr_replace($string, $replacement, $start, $length) - Replaces a portion of a string with another string
+    // echo substr_replace("Replaces a portion of a string with another string", "Replace", 10, 20);
+    // echo substr_replace("Replaces a portion of a string with another string", "Replace", 10); // if length is not given then replace the last entire part from start
+
+
+    // 18. 
+
+
+
+
+
+
 
 
 
