6fb91b4d1b457f258a7476391fe315777c125147
mayanksaini9737 <113924903+mayanksaini9737@users.noreply.github.com>
2024-01-18 12:01:18 +0530

    Given functions practice is completed

--- string_functions.php
+++ string_functions.php
@@ -101,7 +101,89 @@
     // echo substr_replace("Replaces a portion of a string with another string", "Replace", 10); // if length is not given then replace the last entire part from start
 
 
-    // 18. 
+    // 18. str_pad($string, $length, $pad_string, $pad_type) - Pads a string to a certain length with another string
+    // Pad to the right side of the string, to a new length of 30 characters
+    // $pad_string default it is white spaces
+    // $pad_types STR_PAD_BOTH, STR_PAD_LEFT, STR_PAD_RIGHT
+    // $str = "Hello Guys";
+    // echo str_pad($str, 30);
+    // echo str_pad($str, 30, 'how');
+    // echo str_pad($str, 30, 'how', STR_PAD_BOTH);
+
+
+    // 19. strcoll($string1, $string2) - Locale based string comparison
+    // used for performing a string comparison based on the current locale. This function is part of the Internationalization Functions extension (intl), so you need to make sure that the extension is enabled in your PHP configuration
+    // compares two strings 
+    // Note : The strcoll() is case-sensitive but not binary-safe. 
+    // function returns - // 0 - if the two strings are equal
+                        // <0 - if string1 is less than string2
+                        // >0 - if string1 is greater than string2
+    // echo strcoll("Compares Strings", "Compare Strings");
+
+
+    // 20. strcspn($string, $mask, $start, $length) - Finds the length of the initial segment not matching a mask.
+    // returns the number of characters (including whitespaces) found in a string before any part of the specified characters are found
+    // can be useful in filtering out certain characters from the beginning of a string
+    // $length = strcspn("ma 1234yank", "ynk");
+    // $length = strcspn("ma 1234yank", "ank", 5); // its returns number character(after 5 counting starts from 0) before any part that is specified 
+    // echo $length;
+
+    // strspn(string,charlist,optional start,optional length) is also a function as strcspn()
+    // returns the number of characters found in the string that contains only characters from the charlist parameter
+    // used in parsing and validation scenarios where you want to check if the beginning of a string contains only certain types of characters
+    // $length = strspn("mayank123", "abcdefghijklmnopqrstwvxyz");
+    // $length = strspn("mayank123 hemraj", "abcdefghijklmnopqrstwvxyz", 10);
+    // echo $length;
+
+
+    // 21. stristr($haystack, $needle, $before_needle) - Case-insensitive search for the first occurrence of a string
+    // returns the entire part of the string from occurence (from $needle) otherwise it returns FALSE
+    // $before_needle is default false if is set to true then returns the part of string before $needle 
+    // echo stristr("Good Morning", "morning");
+    // echo stristr("Good Morning guys", "oo");
+    // echo "<br>";
+    // echo stristr("Good Morning guys", "oo", true);
+    // echo stristr("Good Morning guys", "gu", true);
+    // For a case-sensitive search, use strstr() function
+
+
+    // 22. strpbrk($string, $char_list) - Searches a string for any of a set of characters
+    // This function is case-sensitive 
+    // This function returns the rest of the string from where it found the first occurrence of a specified character, otherwise it returns FALSE
+    // echo strpbrk("Good morning", "em");
+    // $resutl = strpbrk("Good morning", "eM");
+    // echo var_dump($result); - it prints false
+    // $output = strpbrk("Good morning", "yx");
+    // echo var_dump($output); - it prints false
+
+
+    // 23. strstr($haystack, $needle, $before_needle) - Finds the first occurrence of a string
+    // It is case-sensetive
+    // further same as stristr()
+    // $before_needle is default false if is set to true then returns the part of string before $needle 
+    // echo strstr("Good morning, guys", "mo");
+    // $output = strstr("Good morning, guys", "Mo");
+    // echo var_dump($output);
+    // echo strstr("Good morning, guys", "mo", true);
+
+
+    // 24. strtr($string, $from, $to) or strtr(string,array) - Translates characters or replaces substrings
+    // translates certain characters in a string
+    // If the $from and $to parameters are different in length, both will be formatted to the length of the shortest
+    // echo strtr("replaces substring", "aeiou", "12345");
+    // $arr = array('a'=> 1, 'e'=> 2, 'i'=>3, 'o'=>4, 'u'=>5);
+    // echo strtr("replaces substring", $arr);
+
+
+    // 25. ucfirst($string) - Converts the first character of a string to uppercase
+    // echo ucfirst("converts the first character");
+
+
+    // 26. ucwords($string) - Converts the first character of each word in a string to uppercase
+    // echo ucwords("mayank saini");
+
+
+
 
 
 
